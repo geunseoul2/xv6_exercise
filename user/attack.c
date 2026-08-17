8b69aad1c0e2cadf9f86873093aac0d723b21fe9
이근서 <geunseoul2@gmail.com>
2026-08-17 22:47:17 +0900

    Attack xv6

--- user/attack.c
+++ user/attack.c
@@ -7,6 +7,19 @@ int
 main(int argc, char *argv[])
 {
   // Your code here.
+  
+  // to attack that point, we need to call kalloc in attack..?
+  // to call kalloc, we need to use sbrk() funtion.
+  char* attack;
+  attack = sbrk(PGSIZE);
+  
+  char* target = "This may help.";
+  for(int i=0;i< PGSIZE; i++) {
+    if(memcmp(attack+i, target, 14) == 0) {
+      printf("%s\n",attack+i+16);
+      exit(0);
+    }
+  }
 
   exit(1);
 }
--- user/secret.c
+++ user/secret.c
@@ -22,3 +22,13 @@ main(int argc, char *argv[])
   exit(0);
 }
 
+//After exiting, the secret process becomes a ZOMBIE state,
+//and the parent, which is SH removes the resources of the child process
+//this is when the freeproc function proceeds, which leads to kfree
+//The secret memory is stored in freelist, which is LIFO structure
+
+//Why make the freelist LIFO? -> since there is a L1/L2 cache in CPU
+//it is faster to get the most recently used memory.
+
+//so, to attack that point, we need to call kalloc in attack..?
+
